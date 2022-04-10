7bf705843c133e3296b6af995ea45b8598d142b2
frankappolonia <fappolonia@gmail.com>
2022-04-10 14:52:58 -0400

    Signup and login get route initi

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const home = require('./home')
-//const login = require('./login')
+const login = require('./login')
 //const logout = require('./logout')
-//const signup = require('./signup')
+const signup = require('./signup')
 
 const constructorMethod = (app) => {
   //middleware function
     app.use('/', home);
-    //app.use('/login', login);
+    app.use('/login', login);
     //app.use('/logout', logout);
-    //app.use('/signup', signup)
+    app.use('/signup', signup)
     app.use('*', (req, res) => {
       res.status(404).json({ error: 'PAGE NOT FOUND!' });
     });
---
+++ routes/login.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+router.route('/')
+    .get(async(request, response) =>{
+        try {
+            response.status(200).render('pages/login', {})
+            
+        } catch (e) {
+            response.status(404).json('404: ' + e)
+            
+        }
+    });
+
+module.exports = router
