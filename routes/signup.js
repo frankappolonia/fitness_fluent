fd6e042ab2bdaa3885b8715d62a544ca6d50c2a2
frankappolonia <fappolonia@gmail.com>
2022-04-10 16:43:50 -0400

    added post route for /signup

--- routes/signup.js
+++ routes/signup.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const errorHandling = require('../helper')
+const validations = errorHandling.userValidations
 
 router.route('/')
     .get(async(request, response) =>{
         try {
             response.status(200).render('pages/signup', {})
-            
         } catch (e) {
             response.status(404).json('404: ' + e)
-            
         }
+    })
+    .post(async(request, response)=>{
+
+        try{
+        console.log(request.body.dob)
+
+        validations.signUpRouteValidation(request.body)
+
+        }catch(e){
+            response.status(400).json('400: ' + e)
+        }
+
     });
 
 module.exports = router
