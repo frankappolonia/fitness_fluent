058f42b599bba0107f91988a44e85ad6bb7c5df8
frankappolonia <fappolonia@gmail.com>
2022-04-12 09:47:39 -0400

    Added formulas for nutrition goals
    - Added TDEE calculator
    - Added BMR calculator
    - Added calorie goals calculator (uses TDEE and BMR calculator)

--- data/users.js
+++ data/users.js
@@ -49,6 +49,53 @@ async function createUser(firstName, lastName, email, password, dob, height, ini
       throw 'Could not add new user!'
 }
 
+
+
+function calculateBMR(gender, height, weight, age){
+    /**Calculation for base metabloic rate (cals needed without accounting for activity level) 
+     * Precondition: gender is 'male' or 'female', height is in inches, weight is in lbs
+    */
+   if (arguments.length !== 4) throw "Invalid number of arguments"
+   validations.genderValidation(gender)
+   validations.heightWeightValidation(height, weight)
+   let BMR;
+
+   if (gender === 'male'){
+        BMR = 66 + (6.2 * weight) + (12.7 * height) - (6.76 * age)
+   }
+   else if (gender === 'female'){
+        BMR = 655.1 + (4.35 * weight) + (4.7 * height) - (4.7 * age)
+   }
+   return BMR
+
+}
+
+function calculateTDE(activityLevel, gender, height, weight, age){
+    /**Calculation for total daily energy expenditure (total cals spent each day) */
+    if(arguments.length !== 5) throw "Invalid number of arguments!"
+    validations.activityLevelValidation(activityLevel)
+    let BMR = calculateBMR(gender, height, weight, age)
+
+    let activityLevels = {'sedentary':1.2, 'light':1.375, 'moderate':1.55, 'heavy':1.725, 'hardcore':1.9}
+    let TDEE = activityLevels[activityLevel] * BMR
+    return TDEE
+
+}
+
+function calculateCalsNeeded(weeklyGoal, TDEE){
+    if (arguments.length !== 2) throw "Invaliad number of arguments"
+    validations.weeklyGoalValidation(weeklyGoal)
+
+    switch (weeklyGoal){
+        case -2: return TDEE - 1000 //lose two pounds per week
+        case -1: return TDEE - 500 // lose one pounds per week
+        case -0: return TDEE      // maintain current weight per week
+        case 1: return TDEE + 500 // gain one pounds per week
+        case 2: return TDEE + 1000 // gain two pounds per week
+    }
+
+}
+
 module.exports = {
     createUser
 }
