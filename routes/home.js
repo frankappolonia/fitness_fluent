e36f774db79a8c6cb6f012f4e8f02f55f88bbc3e
frankappolonia <fappolonia@gmail.com>
2022-04-22 09:38:12 -0400

    added partial for daily goals widget

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../data')
+const userFuncs = db.userFuncs
 
 router.route('/')
     .get(async(request, response)=>{
+        let authObj = {authenticated: false}
         try {
-            response.status(200).render('pages/home', {authenticated: request.session})
+            if (request.session.user){
+                authObj.authenticated = true
+                let cals = await userFuncs.getRemainingCalories(request.session.user)
+                authObj['calories'] = cals
+            }
+
+            response.status(200).render('pages/home', authObj)
         } catch (e) {
             response.status(404).json("404: Page cannot be found")
         }
