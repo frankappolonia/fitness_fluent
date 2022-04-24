197b8301a3eca6716077a3537d671062c06bd335
Leelamanoj-06 <68564835+Leelamanoj-06@users.noreply.github.com>
2022-04-24 18:27:47 -0400

    Adding exercise page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const login = require('./login')
 const logout = require('./logout')
 const signup = require('./signup')
 const progress = require('./progress')
+const exercises = require('./exercises')
 
 const constructorMethod = (app) => {
   //middleware function
@@ -11,6 +12,7 @@ const constructorMethod = (app) => {
     app.use('/logout', logout);
     app.use('/signup', signup)
     app.use('/progress', progress)
+    app.use('/exercises', exercises)
     app.use('*', (req, res) => {
       res.status(404).json({ error: 'PAGE NOT FOUND!' });
     });
