8057b31bcced81b38e63ad7a6e7f4574b5c235b6
frankappolonia <fappolonia@gmail.com>
2022-04-26 18:12:30 -0400

    More db functions for forum
    - deletePost()
    - addComment()
    - deleteComment()
    - added comments to posts in the seed.js file

--- data/posts.js
+++ data/posts.js
@@ -7,7 +7,9 @@ const { ObjectId} = require('mongodb');
 
 
 async function addPost(title, body, posterId) {
-    /**Function that creates a new post and adds it to the posts collection */
+    /**Function that creates a new post and adds it to the posts collection 
+     * Returns the post object id
+    */
 
     //1. validate arguments
     if(arguments.length !== 3) throw "Invalid number of arguments!"
@@ -32,11 +34,12 @@ async function addPost(title, body, posterId) {
       comments: []
     };
 
+    //5. insert the new post
     const addPost = await postCollection.insertOne(newPost);
     if (!addPost.insertedId) throw 'Error: post failed!';
 
-    //return this.getPostById(newInsertInformation.insertedId.toString());
-    return "posted successfully!"
+    //6. return post id
+    return addPost.insertedId.toString()
 }
 
 async function getPostById(postId){
@@ -85,10 +88,82 @@ async function deletePost(userId, postId){
     return true
 }
 
+async function addComment(postId, userId, body){
+    /**Adds a comment to an existing post */
+
+    //1. validate 
+    if(arguments.length !== 3) throw "invalid number of arguments"
+    validations.stringChecks([postId, userId, body])
+    validations.stringtrim(arguments)
+    postId = validations.checkId(postId)
+    userId = validations.checkId(userId)
+
+    //2. establish connection to db
+    const postCollection = await posts();
+
+    //3. ensure that the post exists
+    let findPost = await getPostById(postId)
+    if (findPost === null) throw "Post not found!"
+
+    //4. get user info and ensure the user exists
+    let user = await userFuncs.getUserById(userId)
+    if (user == null) throw "User not found!"
+
+    //5. create comment
+    let newComment = {
+        _id: ObjectId(),
+        body: body,
+        poster: {
+          id: userId,
+          name: `${user.firstName} ${user.lastName}`
+            }
+      };
+
+    //6. insert the comment
+    let insertedComment = await postCollection.updateOne({ _id: ObjectId(postId) }, { $push: { comments: newComment} })
+    
+    //7. Check if successfully inserted
+    if (insertedComment.acknowldeged === 0 || !insertedComment.insertedId === 0)
+      throw 'Could not add new comment!'
+    
+    return newComment._id.toString()
+}
+
+async function deleteComment(commentId, posterId){
+    /**Deletes a comment from a particular post. posterId must match the id of the poster in the comment
+     * subdocument
+     */
+
+    //1. validate 
+    if(arguments.length !== 2) throw "Invalid number of arguments!"
+    validations.stringChecks([commentId, posterId])
+    validations.stringtrim(arguments)
+    posterId = validations.checkId(posterId)
+    commentId = validations.checkId(commentId)
+
+     //2. establish connection to db
+     const postCollection = await posts();
+
+     //3. check that the user is authorized to delete the comment
+     let findComment = await postCollection.find({'comments': {$elemMatch: {'_id': ObjectId(commentId)} } }, {projection: {'comments':{$elemMatch: {'_id': ObjectId(commentId)} }, '_id':0 } } ).toArray()
+     if (findComment.length === 0) throw "Error! No comment found with the specified id!" 
+     let poster = findComment[0].comments[0].poster.id.toString()
+     if(poster !== posterId) throw "You are not authorized to delete this comment!"
+
+     //4. delete the comment
+    let removeComment = await postCollection.updateOne({'comments': {$elemMatch: {'_id': ObjectId(commentId)} } }, { $pull: {'comments':{_id: ObjectId(commentId) }}  } )
+    if (removeComment["modifiedCount"] !== 1){
+        return "Error! Could not delete comment!"
+    }
+
+    return true
+}
 
 
 module.exports ={
     addPost,
     getPostById,
-    deletePost
+    deletePost,
+    addComment,
+    deleteComment
 }
