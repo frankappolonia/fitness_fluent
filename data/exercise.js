a04a984e517e9bbcbb590a516b8831291e76d995
Leelamanoj-06 <68564835+Leelamanoj-06@users.noreply.github.com>
2022-04-26 20:47:28 -0400

    Added DB push functionality to exercise..

--- data/exercise.js
+++ data/exercise.js
@@ -1,18 +1,37 @@
-const { usersCollection } = require('../config')
-const db = require('../config')
-const users = db.usersCollection
-const errorHandling = require('../helper')
-const validations = errorHandling.userValidations
-const exerciseFuncs = require('./exerciseFunctions')
+const db = require("../config");
+const users = db.usersCollection;
+const errorHandling = require("../helper");
+const validations = require("../helper/exerciseValidations");
+// const validations = errorHandling.exerciseValidations;
+const userFuncs = require("./users");
+const exerciseFuncs = require("./exerciseFunctions");
+const { ObjectId } = require("mongodb");
 
-async function addExercise(name, calories)
-{
-    if(arguments.length!==2){
-        throw "Incorrect number of arguments!"
+async function addExercise(name, calories, id) {
+  if (arguments.length !== 3) {
+    throw "Incorrect number of arguments!";
+  }
+  let currId = new ObjectId(id);
+  //validations.checkExercise(name);
+  //validations.checkCalories(calories);
+  const usersCollection = await users();
+  const addingExercise = await usersCollection.updateOne(
+    { _id: currId },
+    {
+      $push: {
+        allExercises: {
+          name: name,
+          calories: calories,
+        },
+      },
     }
-    validations.checkExercise(name);
-    validations.checkCalories(calories);
-    const usersCollection = await users()
-    const currUser = usersCollection.findOne
+  );
+  if (!addingExercise.modifiedCount) {
+    throw "Could not add exercise !!!";
+  }
+  return addingExercise;
 }
 
+module.exports = {
+  addExercise,
+};
