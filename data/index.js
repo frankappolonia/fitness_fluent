cd238df7a7c1d43db359b4fc6e610a6ac8dcb302
MaxSurets <maxims2019@gmail.com>
2022-04-29 22:46:50 -0400

    Resolve merge conflict

--- data/index.js
+++ data/index.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 const userFuncs = require("./users");
 const foodFuncs = require("./foodFunctions");
+const postFuncs = require("./posts");
+const exerciseFuncs = require("./exercise");
 
 module.exports = {
   userFuncs: userFuncs,
+  postFuncs: postFuncs,
   foodFuncs: foodFuncs,
+  exerciseFuncs: exerciseFuncs,
 };
-=======
-const userFuncs = require('./users')
-const postFuncs = require('./posts')
-const exerciseFuncs = require('./exercise')
-
-module.exports = {
-    userFuncs: userFuncs,
-    postFuncs: postFuncs,
-    exerciseFuncs: exerciseFuncs
-}
->>>>>>> 8d7bee6ee68a87343ecca9976e0d3bcf70c8edd5
