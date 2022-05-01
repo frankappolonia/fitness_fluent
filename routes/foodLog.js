4da9b194e68a52dfe10ee05d28661bef7ddcbd4b
MaxSurets <maxims2019@gmail.com>
2022-04-30 22:56:50 -0400

    Optional date field for food-log

--- routes/foodLog.js
+++ routes/foodLog.js
@@ -2,11 +2,29 @@ const express = require("express");
 const router = express.Router();
 const data = require("../data");
 const foodFunctions = data.foodFuncs;
+const moment = require("moment");
 
-router.route("/").get(async (request, response) => {
+router.route("/:date?").get(async (request, response) => {
   try {
-    response.status(200).render("pages/food");
+    let date = moment(request.params.date);
+    let dateString = "";
+    if (!date) {
+      dateString = moment().format("YYYY-MM-DD");
+    } else {
+      dateString = moment(date).format("YYYY-MM-DD");
+    }
+    let id = request.session.user;
+    if (!id) {
+      throw "User not found!";
+    }
+    let food = await foodFunctions.getFoodsByDate(id, dateString);
+    response.status(200).render("pages/food", {
+      food: food,
+      date: dateString,
+      script: "/public/js/foodLog.js",
+    });
   } catch (e) {
+    console.error(e);
     response.status(404).json("404: Page cannot be found");
   }
 });
