d0165a8063f25feaea0f836a0148a27d936420b9
Douglas <dpdd64@gmail.com>
2022-05-06 10:50:49 -0400

    routes functions finished

--- routes/profile.js
+++ routes/profile.js
@@ -2,41 +2,135 @@ const express = require("express");
 const router = express.Router();
 const db = require("../data");
 const userFuncs = db.userFuncs;
+const profileFuncs = db.profileFuncs
 const errorHandling = require('../helper');
 const validations = errorHandling.userValidations;
 const xss = require('xss');
 
 //if the user is NOT authenticated, redirect to home
-router.get('/', (request, response, next)=>{
-    if (! request.session.user) {
+router.get('/', (request, response, next) => {
+    if (!request.session.user) {
         return response.redirect('/');
     } else {
         next()
     }
 });
 
-router.route("/")
-    .get(async (request, response) => {
-        try {
-            let authObj = {};
-            //validations
-            let id = validations.checkId(request.session.user);
+router.route("/").get(async (request, response) => {
+    try {
+        let authObj = {};
+        //validations
+        let id = validations.checkId(request.session.user);
 
-            //stuff for daily goals widget
-            authObj.authenticated = true        
-            let nutrients = await userFuncs.getRemainingCalories(id)
-            authObj = {...authObj, ...nutrients}
-            //---------------------------------------
+        //stuff for daily goals widget
+        authObj.authenticated = true
+        let nutrients = await userFuncs.getRemainingCalories(id)
+        authObj = { ...authObj, ...nutrients }
+        //---------------------------------------
 
-            let user = await userFuncs.getUserById(xss(id));
+        let user = await userFuncs.getUserById(xss(id));
 
-            response.status(200).render("pages/profile", {
-                script: "/public/js/profile.js",
-                ...authObj
-            })
-        } catch (e) {
-            response.status(400).render("errors/400", {error: e});
-        }
-    })
+        response.status(200).render("pages/profile", {
+            script: "/public/js/profile.js",
+            ...authObj
+        })
+    } catch (e) {
+        response.status(400).render("errors/400", { error: e });
+    }
+})
+
+router.route("/updateName").patch(async (request, response) => {
+    try {
+        let authObj = {};
+        //validations
+        let id = validations.checkId(request.session.user);
+
+        let firstName = request.body.newFirstName;
+        let lastName = request.body.newLastName;
+        firstName = firstName.trim();
+        lastName = lastName.trim();
+        validations.stringChecks([firstName, lastName]);
+        validations.nameValidation(firstName, lastName);
+
+        profileFuncs.updateName(id, firstName, lastName);
+
+        response.status(200).redirect("/");
+    } catch (e) {
+        response.status(400).render("errors/400", { error: e });
+    }
+})
+
+router.route("/updateActivityLevel").patch(async (request, response) => {
+    try {
+        let authObj = {};
+        //validations
+        let id = validations.checkId(request.session.user);
+
+        let activityLevel = request.body.newActivityLevel;
+        activityLevel = activityLevel.trim();
+        validations.stringChecks([activityLevel]);
+        validations.activityLevelValidation(activityLevel);
+
+        profileFuncs.updateActivityLevel(id, activityLevel);
+
+        response.status(200).redirect("/");
+    } catch (e) {
+        response.status(400).render("errors/400", { error: e });
+    }
+})
+
+router.route("/updateWeeklyWeightGoal").patch(async (request, response) => {
+    try {
+        let authObj = {};
+        //validations
+        let id = validations.checkId(request.session.user);
+
+        let goal = request.body.newGoal;
+        validations.weeklyGoalValidation(goal);
+
+        profileFuncs.updateWeeklyWeightGoal(id, goal);
+
+        response.status(200).redirect("/");
+    } catch (e) {
+        response.status(400).render("errors/400", { error: e });
+    }
+})
+
+router.route("/updateHeight").patch(async (request, response) => {
+    try {
+        let authObj = {};
+        //validations
+        let id = validations.checkId(request.session.user);
+
+        let height = request.body.newHeight;
+        let placeholderWeight = 100;
+        validations.heightWeightValidation(height, placeholderWeight);
+
+        profileFuncs.updateHeight(id, height);
+
+        response.status(200).redirect("/");
+    } catch (e) {
+        response.status(400).render("errors/400", { error: e });
+    }
+})
+
+router.route("/updateWeight").patch(async (request, response) => {
+    try {
+        let authObj = {};
+        //validations
+        let id = validations.checkId(request.session.user);
+
+        let weight = request.body.newWeight;
+        let placeholderHeight = 60;
+        validations.heightWeightValidation(placeholderHeight, weight);
+
+        profileFuncs.updateWeight(id, weight);
+
+        response.status(200).redirect("/");
+    } catch (e) {
+        response.status(400).render("errors/400", { error: e });
+    }
+})
+
 
 module.exports = router;
